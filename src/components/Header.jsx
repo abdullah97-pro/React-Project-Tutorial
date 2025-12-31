eaa5b579d74e25044cf710e4266c289cecbc4223
abdullah97-pro <abdullahahmadi019@gmail.com>
2025-12-31 08:09:10 +0430

    CSS Modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,13 @@
 //     )
 // }
 import './css/header.css'
+import styles from './css/Header.module.css'
+
 function Header({header,content}) {
     return (
         <>
-        <header>{header}</header>
+        <header className={styles.header}>{header}</header>
+        <header className='header'>Testing</header>
         <main>{content}</main>
         </>
     )
